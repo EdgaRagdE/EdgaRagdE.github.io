dae8444456f570718e023c2b1857dca4c85c7e5c
EdgaRagdE <95306434+EdgaRagdE@users.noreply.github.com>
2023-11-07 02:10:17 -0600

    Create imagenes.js

---
+++ src/components/assets/imagenes.js
@@ -0,0 +1,17 @@
+import p1 from './imagenes/Paisaje1.jpg'
+import p2 from './imagenes/Paisaje2.jpg'
+import p3 from './imagenes/Paisaje3.jpg'
+import mi from './imagenes/prueba.jpg'
+import yo from './imagenes/youtube.jpg'
+import na from './imagenes/nasa.jpg'
+import cl from './imagenes/clima.jpg'
+
+export default{
+    "Paisaje1": p1,
+    "Paisaje2": p2,
+    "Paisaje3": p3,
+    "Mifoto": mi,
+    "youtube": yo,
+    "nasa": na,
+    "clima": cl
+}
